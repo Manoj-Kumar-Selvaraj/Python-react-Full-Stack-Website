3c657f72af0fc6265efc3e07984c9730af723f7e
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-17 12:03:22 +0000

    Logic to handle Duplicate Printing

--- factoryoutlet/barcode_print/models.py
+++ factoryoutlet/barcode_print/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class BarcodeT(models.Model):
+    start_barcode = models.BigIntegerField()  # Use BigIntegerField for large integers
+    last_barcode = models.BigIntegerField()   # Use BigIntegerField for large integers
+    print_status=models.BooleanField(default=False)
+    generate_status=models.BooleanField(default=False)
+    dog = models.DateField(null=True, blank=True)
+    print_slot = models.CharField(null=True, default='Y')
+    gen_slot = models.CharField(null=True, default='Y')
+
+    class Meta:
+        db_table = 'BARCODE_T'  # Custom table name
+
+    def __str__(self):
+        return f"Start: {self.start_barcode}, Last: {self.last_barcode}"
--- factoryoutlet/barcode_print/views.py
+++ factoryoutlet/barcode_print/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import BarcodeT
+from django.utils import timezone
+
+def barcode_print_init(barcodes):
+    BarcodeT.objects.create(
+            start_barcode = barcodes[0]
+            last_barcode = barcodes[-1]   # Use BigIntegerField for large integers
+            print_status= False
+            generate_status = True
+            dog = timezone.now()
+            print_slot = 'Y'
+            gen_slot = 'N'
+    )
+
+
 
 # Create your views here.
