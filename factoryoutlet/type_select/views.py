b231fd9ac656ad09781a1563fa705c1c3b8a1ffe
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-22 15:19:01 +0000

    Added auth

--- factoryoutlet/type_select/views.py
+++ factoryoutlet/type_select/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from barcode.models import TypeT
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.decorators import api_view,permission_classes,authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
 from type_add.serializers import TypeTSerializer
