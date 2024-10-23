487a769eb927f8899776445ebfd73788190046d5
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-23 15:59:19 +0000

    Correct the duplicate barcode generation check logic

--- factoryoutlet/barcode_print/utils.py
+++ factoryoutlet/barcode_print/utils.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 
-def return_400_if_object_found(model,number_of_barcodes, **filters):
+def return_400_if_object_found(model1, model2, number_of_barcodes, **filters):
     # Query the model using the filters
-    obj = model.objects.filter(**filters).first()
-    no_of_barcode_calculation=(obj.last_barcode-obj.start_barcode)
+    obj = model1.objects.filter(**filters).first()
+    obj1 = model2.objects.filter(**filters).first()
+    no_of_barcode_calculation=(obj1.last_barcode-obj1.start_barcode)
     
     # If the object is found, return a 400 Bad Request response
     if obj and no_of_barcode_calculation == number_of_barcodes:
