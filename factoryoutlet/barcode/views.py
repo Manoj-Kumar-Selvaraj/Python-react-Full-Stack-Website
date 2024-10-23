3e5a9b4614a9078d1acea7720fa610d701300899
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-23 15:24:22 +0000

    Corrected a syntax error in barcode-view

--- factoryoutlet/barcode/views.py
+++ factoryoutlet/barcode/views.py
@@ -1,4 +1,4 @@
-bfrom django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
