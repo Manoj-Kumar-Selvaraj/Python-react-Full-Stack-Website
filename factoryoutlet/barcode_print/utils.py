5991668e43bf0f9ce0d4fc0d56f946dd6a3c63b9
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-23 18:02:24 +0000

    ref

--- factoryoutlet/barcode_print/utils.py
+++ factoryoutlet/barcode_print/utils.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 def return_400_if_object_found(model1, model2, number_of_barcodes, **filters):
     # Query the model using the filters
     obj = model1.objects.filter(**filters).first()
-    obj1 = model2.objects.filter(obj.b_type).first()
+    obj1 = model2.objects.filter(b_type=obj.b_type).first()
     no_of_barcode_calculation=(obj1.last_barcode-obj1.start_barcode)
     
     # If the object is found, return a 400 Bad Request response
