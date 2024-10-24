101c5008120d0cc551006c7cba7398046fb2b7cf
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-24 22:24:49 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -148,6 +148,7 @@ const Admin = ({ token }) => {
       pseller: DOMPurify.sanitize(pseller),
       pamount: parseFloat(DOMPurify.sanitize(pamount)) 
     };
+      console.log(psize)
       console.log(typeTData)
     }
   
