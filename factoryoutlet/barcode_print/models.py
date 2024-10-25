711ac8d7e2b03acc1c1c9b4f0808c19dbd9e7a41
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-25 19:33:29 +0530

    Updated BarcodeT model

--- factoryoutlet/barcode_print/models.py
+++ factoryoutlet/barcode_print/models.py
@@ -3,14 +3,14 @@ from barcode.models import TypeT
 
 class BarcodeT(models.Model):
     b_type = models.ForeignKey(TypeT, models.DO_NOTHING, db_column='b_type',primary_key=True)
-    number_of_barcodes = models.IntegerField()
+    number_of_barcodes = models.IntegerField(default=0)
     start_barcode = models.BigIntegerField(default=0)  # Use BigIntegerField for large integers
     last_barcode = models.BigIntegerField(default=0)   # Use BigIntegerField for large integers
-    print_status=models.BooleanField(default=False)
-    dog = models.DateField(null=True, blank=True)
+    print_status=models.BooleanField(default=True)
+    dog = models.DateField(default=01/01/0001)
     print_slot = models.CharField(null=True, default='Y',max_length=1)
     gen_slot = models.CharField(null=True, default='Y',max_length=1)
-    Approval = models.CharField(null=True, default='R',max_length=1)
+    Approval = models.CharField(null=True, default='A',max_length=1)
 
     class Meta:
         db_table = 'BARCODE_T'  # Custom table name
