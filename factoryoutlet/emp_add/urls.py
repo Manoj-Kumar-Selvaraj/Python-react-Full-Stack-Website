ac6452dc8f9b128703db94d249d6e604156fea5b
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-25 13:37:12 +0530

    Added urls for employee select

--- factoryoutlet/emp_add/urls.py
+++ factoryoutlet/emp_add/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EmployeeTCreateView
+from .views import EmployeeTCreateView,EmployeeSelectView
 
 urlpatterns = [
     path('access/', EmployeeTCreateView.as_view(), name='create_employee'),
+    path('select/', EmployeeTSelectView.as_view(), name='create_employee'),
     # Other URL patterns
 ]
