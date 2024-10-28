7af4c68fec0cc3e7641de4000406530e8ca758b0
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-10-28 20:30:36 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -112,7 +112,7 @@ const Admin = ({ token }) => {
       console.log(productName)
       const sizeOptionsb = optionsb.filter(option => option.pname === productName).map(option => option.psize);
       setFilteredSizesb([...new Set(sizeOptionsb)]); // Unique sizes
-      console.log(filteredsizesb)
+      console.log(filteredSizesb)
       const typeOptionsb = optionsb.filter(option => option.pname === productName).map(option => option.ptype);
       setFilteredTypesb([...new Set(typeOptionsb)]);
 
