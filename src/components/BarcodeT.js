13100bf2a2dd5a3b5e53b1ab60561dc09ad97440
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-11-01 21:34:26 +0530

    Update BarcodeT.js

--- src/components/BarcodeT.js
+++ src/components/BarcodeT.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './YourModelTable.css';
+import './BarcodeFetch.css';
 
 const BarcodeTTable = ({ token }) => {
   const [data, setData] = useState([]);
