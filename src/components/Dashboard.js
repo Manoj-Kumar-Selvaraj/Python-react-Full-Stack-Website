1fff895f0161b251e953107ece99d2b6a6a85321
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-11-04 00:02:16 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BarcodeTTable from './BarcodeT';
 import EmployeeTTable from './EmployeeT';
+import ProductsTable from './ProductsT';
 
 const Dashboard = ({ token }) => {
   return (
     <>
       <BarcodeTTable token={token} />
       <EmployeeTTable token={token} />
+      <ProductsTable token={token} />
     </>
   );
 };
