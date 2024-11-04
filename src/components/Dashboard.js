13dbe7e172a150af9d3d70b2d7f4b2db68800b2f
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-11-04 21:42:36 +0530

    Update in progress

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,12 +5,24 @@ import ProductsTable from './ProductsT';
 import TypeTable from './TypeT';
 
 const Dashboard = ({ token }) => {
+  const [activeTab, setActiveTab] = useState('home');
+
+    switch (activeTab) {
+      case 'products':
+        return <ProductsTable token={token} />;
+      case 'barcodes':
+        return <BarcodeTTable token={token} />;
+      case 'employees':
+        return <EmployeeTTable token={token} />;
+      case 'admin':
+        return <TypeTable token={token} />;
+      default:
+        return <ProductsTable token={token} />;
+    }
+  };
+
   return (
-    <>
-      <BarcodeTTable token={token} />
-      <EmployeeTTable token={token} />
-      <ProductsTable token={token} />
-      <TypeTable token={token} />
+    <>      
     </>
   );
 };
