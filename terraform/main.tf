1de6c1997329e66ea3e0e50729d1562ed62f1cd0
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2024-12-18 14:34:41 +0000

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,32 @@ resource "aws_dynamodb_table" "terraform_lock_table" {
     type = "S"
   }
 }
+
+resource "aws_s3_bucket" "FactoryOuletFrontEnd" {
+    bucket = "factoryoulet-front-end-host"
+    acl = "private" 
+    website {
+    index_document = "index.html"
+    error_document = "404.html"   # Changed error page
+  }
+   tags = {
+    Name        = "FactoryOulet"
+    Environment = "Production"
+  }
+}
+resource "aws_iam_policy" "FactoryOuletFrontEndPolicy" {
+  name = "factoryoulet-front-end-host-policy"
+  description = "Policy to allow public read access to S3 bucket"
+  policy = jsoncode({
+    version = "2012-10-17"
+    statement = [
+      {
+        Effect   = "Allow"
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::factoryoulet-front-end-host/*"
+      }
+      ]
+    }
+  )
+}
+
