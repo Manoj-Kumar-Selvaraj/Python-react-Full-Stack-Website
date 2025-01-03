cbfce9cc4b360855286aa41d02f65419882affd0
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-03 23:38:23 +0530

    Updated main file with iam user variables

--- terraform/modules/SNS/main.tf
+++ terraform/modules/SNS/main.tf
@@ -2,6 +2,7 @@
 
 module "SNSUSER" {
   source = "./modules/SNSUSER"
+  iam_resources_user_factory_outlet_frontend_developer1 = var.iam_resources_user_factory_outlet_frontend_developer1
   providers = {
     aws = aws.Root
   }
