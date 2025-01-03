edcb7b2489f8f18a72ad3a5a160aee0939927edd
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-03 23:15:24 +0530

    Update outputs.tf

--- terraform/modules/SNS/modules/SNSUSER/outputs.tf
+++ terraform/modules/SNS/modules/SNSUSER/outputs.tf
@@ -1,4 +1,11 @@
-output "topic_arn" {
+output "iam_permissions_user_creation_topic" {
+  description = "ARN of the SNS topic"
+  value       = aws_sns_topic.user_creation_topic
+}
+
+output "iam_permissions_user_creation_topic_arn" {
   description = "ARN of the SNS topic"
   value       = aws_sns_topic.user_creation_topic.arn
 }
+
+
