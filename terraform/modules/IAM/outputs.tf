6292763d425254b710d36bc22a0bdeda264b24dd
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 01:59:42 +0530

    Update outputs.tf

--- terraform/modules/IAM/outputs.tf
+++ terraform/modules/IAM/outputs.tf
@@ -1,9 +1,9 @@
 output "main_lambda_snsfun_lambda_execution_role" {
-  value = module.PERMISSIONSaws_iam_role.lambda_execution_role.id
+  value = module.PERMISSIONS.aws_iam_role.lambda_execution_role
 }
 
-output "main_user" {
-  value = module.RESOURCES.aws_iam_user.factory_outlet_frontend_developer1.name
+output "main_lambda_snsfun_lambda_execution_role_arn" {
+  value = module.PERMISSIONS.aws_iam_role.lambda_execution_role.arn
 }
 
 output "main_sns_snsuser_user_factory_outlet_frontend_developer1" {
