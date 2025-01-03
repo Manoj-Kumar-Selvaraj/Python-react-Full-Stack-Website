38d455dd2501ee7d76e8fd527bad116356e5baf8
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 00:48:43 +0530

    Update main.tf

--- terraform/modules/LAMBDA/main.tf
+++ terraform/modules/LAMBDA/main.tf
@@ -2,10 +2,10 @@
 
 module "RESOURCES" {
   source = "./modules/SNSFUN"
-  iam_permissions_lambda_execution_role = var.iam_permissions_lambda_execution_role
-  iam_permissions_lambda_execution_role_arn = var.iam_permissions_lambda_execution_role_arn
-  s3_s3lam_bucket_lambda_bucket = var.s3_s3lam_bucket_lambda_bucket
-  s3_s3lam_bucket_lambda_bucket_name = var.s3_s3lam_bucket_lambda_bucket_name
+  iam_permissions_lambda_execution_role = var.lambda_iam_permissions_lambda_execution_role
+  iam_permissions_lambda_execution_role_arn = var.lambda_iam_permissions_lambda_execution_role_arn
+  s3_s3lam_bucket_lambda_bucket = var.lambda_s3_s3lam_bucket_lambda_bucket
+  s3_s3lam_bucket_lambda_bucket_name = var.lambda_s3_s3lam_bucket_lambda_bucket_name
   providers = {
     aws = aws.Root
   }
