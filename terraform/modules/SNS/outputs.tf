c8799825ea50e5ddc0ccd923bc0229fc30adf7aa
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-03 23:33:58 +0530

    Added main vairables in sns

---
+++ terraform/modules/SNS/outputs.tf
@@ -0,0 +1,9 @@
+output "_main_iam_permissions_user_creation_topic" {
+  description = "ARN of the SNS topic"
+  value       = module.SNSUSER.aws_sns_topic.user_creation_topic
+}
+
+output "main_iam_permissions_user_creation_topic_arn" {
+  description = "ARN of the SNS topic"
+  value       = module.SNSUSER.aws_sns_topic.user_creation_topic.arn
+}
