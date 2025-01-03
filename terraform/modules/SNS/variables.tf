39a7e85c2a2964e1aab5e3e2495fe746503014c3
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 00:50:06 +0530

    Update variables.tf

--- terraform/modules/SNS/variables.tf
+++ terraform/modules/SNS/variables.tf
@@ -1,4 +1,4 @@
-variable iam_resources_user_factory_outlet_frontend_developer1 {
+variable sns_iam_resources_user_factory_outlet_frontend_developer1 {
   type = string
   description = "User Name"
 }
