dbba1e53f99cd8d93656bae37576802c04d86141
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-03 23:23:19 +0530

    Added main variables

---
+++ terraform/modules/IAM/variables.tf
@@ -0,0 +1,9 @@
+variable main_snsuser_sns_topic {
+  type = string
+  Description = "snsuser module sns topic object for using it in depends on"
+}
+
+variable main_snsuser_sns_topic_arn {
+  type = string
+  Description = "snsuser module sns topic arn for using it in resources"
+}
