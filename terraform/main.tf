f033d8d279051368e8cb07b0ea8948696012caa3
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 01:14:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,8 @@ terraform {
 # IAM Module
 module "IAM" {
   source = "./modules/IAM"
+  iam_snsuser_sns_topic
+  iam_snsuser_sns_topic_arn
   providers = {
     aws = aws.Root
   }
@@ -19,7 +21,8 @@ module "IAM" {
 
 module "SNS" {
   source = "./modules/SNS"
-  depends_on = [module.RESOURCES]
+  depends_on = [module.IAM]
+  sns_iam_resources_user_factory_outlet_frontend_developer1
   user = module.RESOURCES.user
   providers = {
     aws = aws.Root
@@ -30,7 +33,11 @@ module "SNS" {
 
 module "LAMBDA" {
   source = "./modules/LAMBDA"
-  depends_on = [module.RESOURCES]
+  depends_on = [module.IAM,module.SNS,module.S3]
+  lambda_iam_permissions_lambda_execution_role = main_lambda_snsfun_lambda_execution_role
+  lambda_iam_permissions_lambda_execution_role_arn = main_lambda_snsfun_lambda_execution_role_
+  lambda_s3_s3lam_bucket_lambda_bucket = 
+  lambda_s3_s3lam_bucket_lambda_bucket_name
   user = module.RESOURCES.user
   providers = {
     aws = aws.Root
@@ -41,7 +48,7 @@ module "LAMBDA" {
 
 module "S3" {
   source = "./modules/S3"
-  depends_on = [module.RESOURCES]
+  depends_on = [module.IAM]
   user = module.RESOURCES.user
   providers = {
     aws = aws.Root
