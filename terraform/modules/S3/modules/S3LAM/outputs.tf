b351c5e808014d3a18db3b95aae987fab6147363
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 16:34:48 +0530

    Update outputs.tf

--- terraform/modules/S3/modules/S3LAM/outputs.tf
+++ terraform/modules/S3/modules/S3LAM/outputs.tf
@@ -1,3 +1,6 @@
-output "lambda_snsfun_s3_lambda_bucket" {
+output "lambda_snsfun_s3_lambda_bucket_name" {
    value = aws_s3_bucket.lambda_bucket.bucket
+}
+output "lambda_snsfun_s3_lambda_bucket" {
+   value = aws_s3_bucket.lambda_bucket
 }
