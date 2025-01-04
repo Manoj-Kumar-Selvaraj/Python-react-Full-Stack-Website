3688a10e03059c5ac53a99a0cc3bc96e2c0d8f35
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 22:50:12 +0530

    Update main.tf

--- terraform/modules/IAM/main.tf
+++ terraform/modules/IAM/main.tf
@@ -10,6 +10,7 @@ module "RESOURCES" {
 # CALLING RESOURCE MODULE
 
 module "PERMISSIONS" {
+  depends_on = [module.RESOURCES]
   source = "./modules/PERMISSIONS"
   snsuser_sns_topic_arn = var.iam_sns_snsuser_sns_topic_arn
   providers = {
