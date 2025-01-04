ae714383410bda16a134a33eb70ed1c0a155af42
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 22:45:19 +0530

    Added defaults

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,25 +6,30 @@ variable iam_sns_snsuser_sns_topic_arn {
 
 variable lambda_iam_permissions_lambda_execution_role {
   type = string
+  default = " "
   description = "lambda role object"
 }
 
 variable lambda_iam_permissions_lambda_execution_role_arn {
+  default = "*"
   type = string
   description = "lambda role object arn"
 }
 
 variable lambda_s3_s3lam_bucket_lambda_bucket {
+  default = " "
   type = string
   description = "s3lam bucket object"
 }
 
 variable lambda_s3_s3lam_bucket_lambda_bucket_name {
+  default = " "
   type = string
   description = "s3lam bucket name"
 }
 
 variable sns_iam_resources_user_factory_outlet_frontend_developer1 {
+  default = " "
   type = string
   description = "User Name"
 }
