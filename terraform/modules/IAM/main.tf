5b0fc1c1d2dd10dacd7c00bf1eadc53f0dfc2cb0
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 22:09:11 +0530

    Update main.tf

--- terraform/modules/IAM/main.tf
+++ terraform/modules/IAM/main.tf
@@ -11,7 +11,6 @@ module "RESOURCES" {
 
 module "PERMISSIONS" {
   source = "./modules/PERMISSIONS"
-  snsuser_sns_topic = var.iam_sns_snsuser_sns_topic
   snsuser_sns_topic_arn = var.iam_sns_snsuser_sns_topic_arn
   providers = {
     aws = aws.Root
