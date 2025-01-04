c00c2a5ade4a6cc17ede1b385f23c718e4eaef1b
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 22:04:54 +0530

    Update main.tf

--- terraform/modules/SNS/modules/SNSUSER/main.tf
+++ terraform/modules/SNS/modules/SNSUSER/main.tf
@@ -7,7 +7,6 @@ resource "aws_sns_topic" "user_creation_topic" {
 }
 
 resource "aws_sns_topic_subscription" "user_creation_email_subscription" {
-  depends_on = [aws_sns_topic.user_creation_topic]
   topic_arn = aws_sns_topic.user_creation_topic.arn
   protocol  = "email"
   endpoint  = "ss.mano1998@gmail.com"
