bd9431c70ec536a20919fe8648a25c15bbcee5a5
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 22:09:24 +0530

    Update variables.tf

--- terraform/modules/IAM/variables.tf
+++ terraform/modules/IAM/variables.tf
@@ -1,8 +1,3 @@
-variable iam_sns_snsuser_sns_topic {
-  type = string
-  description = "snsuser module sns topic object for using it in depends on"
-}
-
 variable iam_sns_snsuser_sns_topic_arn {
   type = string
   description = "snsuser module sns topic arn for using it in resources"
