6782fbc8a6f5e5b0e9c68c036b51661627f5f4ae
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 16:33:15 +0530

    Added depends on

--- terraform/modules/S3/modules/S3LAM/main.tf
+++ terraform/modules/S3/modules/S3LAM/main.tf
@@ -7,6 +7,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 }
 
 resource "aws_s3_object" "lambda_code" {
+  depends_on = [aws_s3_bucket.lambda_bucket]
   bucket = aws_s3_bucket.lambda_bucket.bucket
   key    = "sns_lambda_function.zip"
   source = "sns_lambda_function.zip"
