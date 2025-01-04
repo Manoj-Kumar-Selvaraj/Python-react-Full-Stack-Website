48b92fb5ea61f425998fd40af3c753cb1035168f
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 16:03:18 +0530

    Update outputs.tf

--- terraform/modules/SNS/outputs.tf
+++ terraform/modules/SNS/outputs.tf
@@ -1,4 +1,4 @@
-output "_main_iam_permissions_user_creation_topic" {
+output "main_iam_permissions_user_creation_topic" {
   description = "ARN of the SNS topic"
   value       = module.SNSUSER.aws_sns_topic.user_creation_topic
 }
