ab014ec28274b002636b7831ec4ee0f275415825
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-04 16:44:35 +0530

    Update outputs.tf

--- terraform/modules/S3/outputs.tf
+++ terraform/modules/S3/outputs.tf
@@ -1,9 +1,9 @@
 output "main_lambda_snsfun_s3_lambda_bucket_name" {
-   value = aws_s3_bucket.lambda_bucket.bucket
+   value = module.S3LAM.lambda_snsfun_s3_lambda_bucket_name
 }
 
 output "main_lambda_snsfun_s3_lambda_bucket" {
-   value = aws_s3_bucket.lambda_bucket
+   value = module.S3LAM.lambda_snsfun_s3_lambda_bucket
 }
 
 
