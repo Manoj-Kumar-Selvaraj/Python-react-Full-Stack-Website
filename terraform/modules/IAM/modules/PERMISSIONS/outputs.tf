b5b60800c8cdc992419a44d3d5df2ab8e579787b
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-06 15:54:11 +0530

    Update outputs.tf

--- terraform/modules/IAM/modules/PERMISSIONS/outputs.tf
+++ terraform/modules/IAM/modules/PERMISSIONS/outputs.tf
@@ -1,3 +1,9 @@
 output "lambda_snsfun_lambda_execution_role" {
   value = aws_iam_role.lambda_execution_role.id
 }
+
+output "lambda_snsfun_lambda_execution_role_arn" {
+  value = aws_iam_role.lambda_execution_role.arn
+}
+
+
