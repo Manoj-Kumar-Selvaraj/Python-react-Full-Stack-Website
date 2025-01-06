64ecb5c7bbc211f70f0cf66c7719c891e2bf8974
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-06 14:27:09 +0530

    Update main.tf

--- terraform/modules/SNS/main.tf
+++ terraform/modules/SNS/main.tf
@@ -3,7 +3,4 @@
 module "SNSUSER" {
   source = "./modules/SNSUSER"
   iam_resources_user_factory_outlet_frontend_developer1 = var.sns_iam_resources_user_factory_outlet_frontend_developer1
-  providers = {
-    aws = aws.Root
-  }
 }
