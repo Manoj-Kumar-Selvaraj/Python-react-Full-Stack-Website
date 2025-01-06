b54a7584e27cf0d5ec1575db43a29b263300b241
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-01-06 14:26:19 +0530

    Update main.tf

--- terraform/modules/LAMBDA/main.tf
+++ terraform/modules/LAMBDA/main.tf
@@ -6,7 +6,4 @@ module "RESOURCES" {
   iam_permissions_lambda_execution_role_arn = var.lambda_iam_permissions_lambda_execution_role_arn
   s3_s3lam_bucket_lambda_bucket = var.lambda_s3_s3lam_bucket_lambda_bucket
   s3_s3lam_bucket_lambda_bucket_name = var.lambda_s3_s3lam_bucket_lambda_bucket_name
-  providers = {
-    aws = aws.Root
-  }
 }
