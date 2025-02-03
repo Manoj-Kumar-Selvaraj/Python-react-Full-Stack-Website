d92859090be058d83205205fa8049aa0c1b59c95
ManojKumarSelvaraj-2 <manojkumarawstest1@gmail.com>
2025-02-03 19:12:12 +0000

    Added tooltip feature

--- terraform-user-setup/Diagram-As-Code.py
+++ terraform-user-setup/Diagram-As-Code.py
@@ -128,7 +128,10 @@ def create_diagram(services, dependency_matrix, output_file="output_diagram"):
                         icon = get_icon(service_type)
                         if icon:
                             # node = icon(f"\n[{resource_name}]", fontsize=ICON_SIZE, shape="box", width="0.5", height="0.4", tooltip=f"{resource_name} ({service_type})")
-                            node = icon(f"\n", fontsize=ICON_SIZE, shape="box", width="0.5", height="0.4", tooltip=f"{resource_name} ({service_type})")
+                            # node = icon(f"\n", fontsize=ICON_SIZE, shape="box", width="0.5", height="0.4", tooltip=f"{resource_name} ({service_type})")
+                            # Create the node with no label, only a tooltip
+                            node = icon("", fontsize=ICON_SIZE, shape="box", width="0.5", height="0.4")
+                            node.attr(tooltip=f"{resource_name} ({service_type})")  # Tooltip will display on hover
                             sub_nodes.append(node)
                     cluster_nodes.update({item[1]: sub_nodes[i] for i, item in enumerate(items)})
 
