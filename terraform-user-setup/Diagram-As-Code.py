32c8ac057d4f2b021f42abfb46045a28c4b6a2a6
ManojKumarSelvaraj-2 <manojkumarawstest1@gmail.com>
2025-02-04 12:34:20 +0000

    04/02-3rd

--- terraform-user-setup/Diagram-As-Code.py
+++ terraform-user-setup/Diagram-As-Code.py
@@ -141,7 +141,7 @@ def create_diagram(services, dependency_matrix, output_file="output_diagram"):
                         if icon:
                             # Create the node with no label, only a tooltip
                             # icon = icon(f"{resource_name}\n{service_type}", fontsize=ICON_SIZE, shape="box", width="0.5", height="0.4")
-                            icon = icon(label=f"<<B>{resource_name}</B>>", href=f"javascript:alert('{service_type}')",
+                            icon = icon("", href=f"javascript:showTooltip(event, '{resource_name}', '{service_type}')",
                                 shape="box", width="0.5", height="0.4")
                             # node = Custom(f"{resource_name}","https://factoryoutlet-aws-diagrams-resources.s3.us-east-1.amazonaws.com/resources/aws/category/service_name",tooltip=f"{resource_name} ({service_type})")
                             sub_nodes.append(icon)
