2011bba779b9d3470595976c8c079488cd5e1914
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-03-14 18:39:47 +0000

    refresh

--- USER-CREATION-NOTIFICATION-ACC2/providers.tf
+++ USER-CREATION-NOTIFICATION-ACC2/providers.tf
@@ -7,3 +7,13 @@ terraform {
   }
   required_version = ">= 1.9.8" # Ensure compatibility with your Terraform version
 }
+
+provider "aws" {
+  alias   = "Root"
+  region  = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::039612868338:role/FactoryOutlet-Front-End-Root-Role"
+    session_name = "USERCREATIONNOTIFICATIONS"
+  } 
+  profile = "default"   
+}
--- USER-CREATION-NOTIFICATION/modules/IAM/modules/RESOURCES/variables.tf
+++
@@ -1,7 +0,0 @@
-variable user_name {
-    type = string
-}
-
-variable group_name {
-    type = string
-}
